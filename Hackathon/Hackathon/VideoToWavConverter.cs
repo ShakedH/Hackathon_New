b11a6c0869517a20117b638f5fa457c6998942a7
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-03-30 16:25:54 +0300

    Roniel

--- Hackathon/Hackathon/VideoToWavConverter.cs
+++ Hackathon/Hackathon/VideoToWavConverter.cs
@@ -1,36 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System.IO;
+using System.Diagnostics;
+using System;
 
 namespace Hackathon
 {
     public class VideoToWavConverter
     {
         public string VideoFilePath { get; private set; }
-        public string OutputFileName { get; set; }
+        public string OutputAudioPath { get; set; }
+        public string OutputFullPath { get; set; }
         public string CommandLine { get; private set; }
-        public string ffmpegPath
-        {
-            get { return @"C:\ffmpeg\bin\"; }
-            private set { }
-        }
 
-        public VideoToWavConverter(string oVideoFilePath, string oOutoutFileName)
+        public VideoToWavConverter(string inputVideoPath, string outputAudioPath)
         {
-            VideoFilePath = oVideoFilePath;
-            OutputFileName = oOutoutFileName;
-            CommandLine = "C:\\ffmpeg\\bin\\ffmpeg -i \" \"";
+            if (!File.Exists(inputVideoPath))
+                throw new ArgumentException("Input video file path is not valid");
+            VideoFilePath = inputVideoPath;
+            OutputAudioPath = outputAudioPath;
+            if (!OutputAudioPath.EndsWith(@"\"))
+                OutputAudioPath += @"\";
+            OutputFullPath = OutputAudioPath + "output_audio.wav";
+            CommandLine = string.Format("C:\\ffmpeg\\bin\\ffmpeg -i \"{0}\" \"{1}\"", VideoFilePath, OutputFullPath);
         }
 
         public void Convert()
         {
-            throw new NotImplementedException();
-            using (StreamWriter sw = new StreamWriter("executeConversion.bat"))
+            string batchFileName = "converter.bat";
+            using (StreamWriter sw = new StreamWriter(batchFileName))
             {
+                sw.WriteLine(CommandLine);
             }
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + CommandLine);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            Process p = Process.Start(processInfo);
+            p.WaitForExit();
         }
     }
 }
