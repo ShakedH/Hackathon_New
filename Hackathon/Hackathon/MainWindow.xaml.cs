4e6216e0aa6bfb5f3939fe1a8faf375132312129
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-03-31 00:41:35 +0300

    Roniel

--- Hackathon/Hackathon/MainWindow.xaml.cs
+++ Hackathon/Hackathon/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Hackathon
 {
@@ -21,13 +22,10 @@ namespace Hackathon
     public partial class MainWindow : Window
     {
         private const string _defaultText = "Search in our videos...";
+
         public MainWindow()
         {
             InitializeComponent();
-            //SearchScreen.Visibility = Visibility.Visible;
-            //ResultsScreen.Visibility = Visibility.Hidden;
-            SearchScreen.Visibility = Visibility.Hidden;
-            ResultsScreen.Visibility = Visibility.Visible;
             searchBox.Foreground = Brushes.LightSlateGray;
             searchBox.Text = _defaultText;
             searchBox.VerticalContentAlignment = VerticalAlignment.Center;
@@ -38,6 +36,11 @@ namespace Hackathon
             searchBox.Foreground = Brushes.Black;
             if (searchBox.Text == _defaultText)
                 searchBox.Text = string.Empty;
+        }
+
+        private void searchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new VideoWindow().Show();
         }
     }
 }
---
+++ Hackathon/Hackathon/VideoWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Hackathon
+{
+    /// <summary>
+    /// Interaction logic for VideoWindow.xaml
+    /// </summary>
+    public partial class VideoWindow : Window
+    {
+        public VideoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private DispatcherTimer timerVideoTime;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timerVideoTime = new DispatcherTimer();
+            timerVideoTime.Interval = TimeSpan.FromSeconds(0.1);
+            timerVideoTime.Tick += new EventHandler(timer_Tick);
+            minionPlayer.Stop();
+        }
+
+        private void minionPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            sbarPosition.Minimum = 0;
+            sbarPosition.Maximum = minionPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+            sbarPosition.Visibility = Visibility.Visible;
+            Duration.Text = minionPlayer.NaturalDuration.TimeSpan.ToString();
+        }
+
+        private void ShowPosition()
+        {
+            sbarPosition.Value = minionPlayer.Position.TotalSeconds;
+            txtPosition.Text = string.Format("{0:hh\\:mm\\:ss}", minionPlayer.Position);
+        }
+
+        private void EnableButtons(bool is_playing)
+        {
+            if (is_playing)
+            {
+                btnPlay.IsEnabled = false;
+                btnPause.IsEnabled = true;
+                btnPlay.Opacity = 0.5;
+                btnPause.Opacity = 1.0;
+            }
+            else
+            {
+                btnPlay.IsEnabled = true;
+                btnPause.IsEnabled = false;
+                btnPlay.Opacity = 1.0;
+                btnPause.Opacity = 0.5;
+            }
+            timerVideoTime.IsEnabled = is_playing;
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Play();
+            EnableButtons(true);
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Pause();
+            EnableButtons(false);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Stop();
+            EnableButtons(false);
+            ShowPosition();
+        }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Stop();
+            minionPlayer.Play();
+            EnableButtons(true);
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Position += TimeSpan.FromSeconds(10);
+            ShowPosition();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            minionPlayer.Position -= TimeSpan.FromSeconds(10);
+            ShowPosition();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ShowPosition();
+        }
+
+        private void searchBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
