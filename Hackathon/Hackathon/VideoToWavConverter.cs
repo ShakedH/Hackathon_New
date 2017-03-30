cb27079dc8efd3b142f350c7d69e796c32c44a19
שקד חזון <hazons@post.bgu.ac.il>
2017-03-30 15:00:46 +0300

    First Commit

---
+++ Hackathon/Hackathon/VideoToWavConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hackathon
+{
+    public class VideoToWavConverter
+    {
+        public string VideoFilePath { get; private set; }
+        public string OutputFileName { get; set; }
+        public string CommandLine { get; private set; }
+        public string ffmpegPath
+        {
+            get { return @"C:\ffmpeg\bin\"; }
+            private set { }
+        }
+
+        public VideoToWavConverter(string oVideoFilePath, string oOutoutFileName)
+        {
+            VideoFilePath = oVideoFilePath;
+            OutputFileName = oOutoutFileName;
+            CommandLine = "C:\\ffmpeg\\bin\\ffmpeg -i \" \"";
+        }
+
+        public void Convert()
+        {
+            //using (StreamWriter sw = new StreamWriter("executeConversion.bat"))
+            //{
+            //    string 
+            //}
+        }
+    }
+}
---
+++ Hackathon/HackathonTests/APIClientTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hackathon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Tests
+{
+    [TestClass()]
+    public class APIClientTests
+    {
+        [TestMethod()]
+        public void ShortConversionWithTags()
+        {
+            string file = @"C:\Users\user\Desktop\Hack\s3.wav";
+            string response = "";
+            try
+            {
+                APIClient client = new APIClient();
+                response = client.Convert(file);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Assert.Fail();
+            }
+            Assert.IsTrue(response.StartsWith("Right now look fucker"));
+            Console.WriteLine(response);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FileNotExist()
+        {
+            string file = @"C:\Users\user\Desktop\Hack\NotExistFile.wav";
+            string response = "";
+            APIClient client = new APIClient();
+            response = client.Convert(file);
+        }
+    }
+}
