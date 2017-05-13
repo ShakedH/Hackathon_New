03c5e613436cf9277f0cce798f018849d3c25fb0
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-05-14 02:41:28 +0300

    Automated MainIndex creation. In MainIndexTest. Run Method1 and then Method2...

--- Hackathon/HackathonTests/MainIndexTest.cs
+++ Hackathon/HackathonTests/MainIndexTest.cs
@@ -13,31 +13,84 @@ namespace HackathonTests
         [TestMethod]
         public void TestMethod1()
         {
-            Dictionary<string, List<VideoDetails>> dic = new Dictionary<string, List<VideoDetails>>();
-            List<string> keywords = new List<string>();
-            keywords.Add("communication");
-            keywords.Add("network");
-            keywords.Add("transport");
-            keywords.Add("application");
-            keywords.Add("internet");
-            VideoDetails vd1 = new VideoDetails("Communication", keywords);
-            List<VideoDetails> vdList = new List<VideoDetails>();
-            vdList.Add(vd1);
-            dic["communication"] = vdList;
-            dic["network"] = vdList;
-            dic["transport"] = vdList;
-            dic["internet"] = vdList;
-            dic["application"] = vdList;
-            dic["layers"] = vdList;
-            dic["layer"] = vdList;
-            dic["link"] = vdList;
-            dic["data"] = vdList;
-            dic["address"] = vdList;
+            Dictionary<string, List<VideoDetails>> mainIndex = new Dictionary<string, List<VideoDetails>>();
+            //byte[] index = Hackathon.Properties.Resources.MainIndex;
+            //using (Stream stream = new MemoryStream(index))
+            //{
+            //    BinaryFormatter reader = new BinaryFormatter();
+            //    mainIndex = (Dictionary<string, List<VideoDetails>>)reader.Deserialize(stream);
+            //}
 
+            string fileName = "Communication";
+            Video video = Video.LoadVideoFromResource(fileName);
+            video.Metadata = new VideoMetadata("Engineering", "Information Sys. Engineering", "Communication 101", "Dr. Gal Shpitz");
+            List<string> keywords = new List<string>(video.GetMostFrequentStrings(5).Keys);
+            VideoDetails vd = new VideoDetails(fileName, keywords);
+            foreach (string term in video.Terms.Keys)
+            {
+                if (!mainIndex.ContainsKey(term))
+                    mainIndex[term] = new List<VideoDetails>();
+                mainIndex[term].Add(vd);
+            }
             using (FileStream stream = File.Open("MainIndex.bin", FileMode.Create))
             {
                 BinaryFormatter writer = new BinaryFormatter();
-                writer.Serialize(stream, dic);
+                writer.Serialize(stream, mainIndex);
+            }
+            //Dictionary<string, List<VideoDetails>> dic = new Dictionary<string, List<VideoDetails>>();
+            //List<string> keywords = new List<string>();
+            //keywords.Add("communication");
+            //keywords.Add("network");
+            //keywords.Add("transport");
+            //keywords.Add("application");
+            //keywords.Add("internet");
+            //VideoDetails vd1 = new VideoDetails("Communication", keywords);
+            //List<VideoDetails> vdList = new List<VideoDetails>();
+            //vdList.Add(vd1);
+            //dic["communication"] = vdList;
+            //dic["network"] = vdList;
+            //dic["transport"] = vdList;
+            //dic["internet"] = vdList;
+            //dic["application"] = vdList;
+            //dic["layers"] = vdList;
+            //dic["layer"] = vdList;
+            //dic["link"] = vdList;
+            //dic["data"] = vdList;
+            //dic["address"] = vdList;
+
+            //using (FileStream stream = File.Open("MainIndex.bin", FileMode.Create))
+            //{
+            //    BinaryFormatter writer = new BinaryFormatter();
+            //    writer.Serialize(stream, dic);
+            //}
+        }
+
+        [TestMethod]
+        public void UpdateIndexWithNewVideo()
+        {
+            Dictionary<string, List<VideoDetails>> mainIndex;
+            byte[] index = Hackathon.Properties.Resources.MainIndex;
+            using (Stream stream = new MemoryStream(index))
+            {
+                BinaryFormatter reader = new BinaryFormatter();
+                mainIndex = (Dictionary<string, List<VideoDetails>>)reader.Deserialize(stream);
+            }
+
+            string fileName = "ClimateChange";
+            Video video = Video.LoadVideoFromResource(fileName);
+            video.Metadata = new VideoMetadata("Natural Science", "Geology", "Climate Changes", "Prof. Dan Shapira");
+            List<string> keywords = new List<string>(video.GetMostFrequentStrings(5).Keys);
+            VideoDetails vd = new VideoDetails(fileName, keywords);
+            foreach (string term in video.Terms.Keys)
+            {
+                if (!mainIndex.ContainsKey(term))
+                    mainIndex[term] = new List<VideoDetails>();
+                mainIndex[term].Add(vd);
+            }
+            using (FileStream stream = File.Open("MainIndex.bin", FileMode.Create))
+            {
+                BinaryFormatter writer = new BinaryFormatter();
+                writer.Serialize(stream, mainIndex);
             }
         }
     }
